aa832569b6a688b34ea8e58cdd1e06885d2aa41d
BuduschiyHokage <kolya.skiba.2013@gmail.com>
2022-02-19 21:46:16 +0300

    separate og from whitelist and make internal vars public

--- contracts/contracts/Minting.sol
+++ contracts/contracts/Minting.sol
@@ -11,12 +11,13 @@ interface IERC721Custom {
 contract Minting is Ownable {
     IERC721Custom public nft;
     uint256 public startDate;
-    bytes32 whiteListRoot;
-    bytes32 OGRoot;
+    bytes32 public whitelistRoot;
+    bytes32 public OGRoot;
 
     uint256 public ogQuantity = 4;
     uint256 public whitlistQuantity = 2;
     uint256 public maxQuantity = 20;
+    uint256 public duration = 7 days;
 
     mapping(address => uint256) public minted;
 
@@ -24,26 +25,7 @@ contract Minting is Ownable {
     /// @param proof array of bytes for merkle tree verifing
     /// @param root tree's root
     /// @param leaf keccak256 of user address
-    function verifyOG(
-        bytes32[] memory proof,
-        bytes32 root,
-        bytes32 leaf
-    ) public pure returns (bool) {
-        bytes32 hash = leaf;
-        for (uint256 i = 0; i < proof.length; i++) {
-            bytes32 proofElement = proof[i];
-            hash = hash < proofElement
-                ? keccak256(abi.encodePacked(hash, proofElement))
-                : keccak256(abi.encodePacked(proofElement, hash));
-        }
-        return hash == root;
-    }
-
-    /// @dev verifies whitelist
-    /// @param proof array of bytes for merkle tree verifing
-    /// @param root tree's root
-    /// @param leaf keccak256 of user address
-    function verifyWhitelist(
+    function verify(
         bytes32[] memory proof,
         bytes32 root,
         bytes32 leaf
@@ -64,14 +46,14 @@ contract Minting is Ownable {
     function mint(uint256 quantity, bytes32[] memory proof) external {
         console.log('start date', startDate);
         require(
-            (startDate != 0) && (block.timestamp < startDate + 7 days),
+            (startDate != 0) && (block.timestamp < startDate + duration),
             'Mint have not started yet'
         );
 
         if (block.timestamp - startDate < 1 days) {
             bytes32 leaf = keccak256(abi.encode(msg.sender));
-            bool isOG = verifyOG(proof, OGRoot, leaf);
-            bool isWhitelist = verifyWhitelist(proof, whiteListRoot, leaf);
+            bool isOG = verify(proof, OGRoot, leaf);
+            bool isWhitelist = verify(proof, whitelistRoot, leaf);
 
             console.log(isOG, isWhitelist);
 
@@ -108,12 +90,21 @@ contract Minting is Ownable {
         startDate = 0;
     }
 
-    /// @dev Set whitelist
-    /// @param _whiteListRoot whitelist's tree root
+    /// @dev Call start minting event
+    function changeDuration(uint256 _duration) external onlyOwner {
+        duration = _duration;
+    }
+
+    /// @dev Set whitelist Root
     /// @param _OGRoot OG's tree root
-    function createWhitelist(bytes32 _whiteListRoot, bytes32 _OGRoot) external onlyOwner {
-        whiteListRoot = _whiteListRoot;
+    function saveRootOg(bytes32 _OGRoot) external onlyOwner {
         OGRoot = _OGRoot;
+    }
+
+    /// @dev Set whitelist Root
+    /// @param _whitelistRoot whitelist's tree root
+    function saveRootWhitelist(bytes32 _whitelistRoot) external onlyOwner {
+        whitelistRoot = _whitelistRoot;
     }
 
     constructor(address nft_) {
