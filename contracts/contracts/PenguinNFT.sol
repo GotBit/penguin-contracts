c3af2537a026fba7d20d312b2182765abe17b9b0
BuduschiyHokage <kolya.skiba.2013@gmail.com>
2022-02-19 22:05:15 +0300

    add max supply to token and keccak to role

--- contracts/contracts/PenguinNFT.sol
+++ contracts/contracts/PenguinNFT.sol
@@ -6,30 +6,35 @@ import '@openzeppelin/contracts/access/AccessControl.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
 
 contract PenguinNFT is ERC721A, AccessControl, Ownable {
-    bytes32 public constant minter = 'minter';
+    bytes32 public constant MINTER_ROLE = keccak256('MINTER_ROLE');
     string public baseURI;
+    uint256 public minted;
+    uint256 public maxSupply;
 
-    constructor() ERC721A('Genesis Penguin', 'GP') {
+    constructor(uint256 maxSupply_) ERC721A('Genesis Penguin', 'GP') {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        maxSupply = maxSupply_;
     }
 
     /// @dev mint
     /// @param quantity amount to mint
-    function mint(address user, uint256 quantity) external payable onlyRole(minter) {
+    function mint(address user, uint256 quantity) external payable onlyRole(MINTER_ROLE) {
         // _safeMint's second argument now takes in a quantity, not a tokenId.
+        require((quantity + minted) <= maxSupply);
         _safeMint(user, quantity);
+        minted += quantity;
     }
 
     /// @dev set minter address
     /// @param _minter minter address
     function setMinter(address _minter) external onlyOwner {
-        grantRole(minter, _minter);
+        grantRole(MINTER_ROLE, _minter);
     }
 
     /// @dev revoke minter address
     /// @param _minter minter address
     function revokeMinter(address _minter) public {
-        revokeRole(minter, _minter);
+        revokeRole(MINTER_ROLE, _minter);
     }
 
     function setURI(string memory uri) public onlyOwner {
