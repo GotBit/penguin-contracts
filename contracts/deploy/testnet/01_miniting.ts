2b80e04c1ec3d2e7d711dbe88c3b56fbead96bc2
BuduschiyHokage <kolya.skiba.2013@gmail.com>
2022-02-22 00:01:29 +0300

    add another chains

--- contracts/deploy/testnet/01_miniting.ts
+++ contracts/deploy/testnet/01_miniting.ts
@@ -5,9 +5,10 @@ import chainlink from '../../chainlink.json'
 import { ethers } from 'hardhat'
 import { Minting, PenguinNFT } from '../../typechain'
 
-const chain = chainlink.bsc_testnet
-
-const router = '0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3'
+const routers = {
+  rinkeby: '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D',
+  bsc_testnet: '0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3',
+} as any
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments } = hre
@@ -15,10 +16,17 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const [deployer, _, bank] = await ethers.getSigners()
 
   const nft = (await ethers.getContract('PenguinNFT')) as PenguinNFT
-
+  const chain = (chainlink as any)[hre.network.name]
   await deploy('Minting', {
     from: deployer.address,
-    args: [nft.address, router, chain.VRF, chain.LINK, chain.key, chain.fee],
+    args: [
+      nft.address,
+      routers[hre.network.name],
+      chain.VRF,
+      chain.LINK,
+      chain.key,
+      chain.fee,
+    ],
     log: true,
   })
 }
