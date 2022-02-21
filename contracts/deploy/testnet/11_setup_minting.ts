8ca19ad50bf9bab53e1062499eb1913395111ef5
BuduschiyHokage <kolya.skiba.2013@gmail.com>
2022-02-21 18:45:07 +0300

    fix root  error

--- contracts/deploy/testnet/11_setup_minting.ts
+++ contracts/deploy/testnet/11_setup_minting.ts
@@ -7,6 +7,12 @@ import { Minting, PenguinNFT } from '../../typechain'
 const addressOG = '0x4E1602505a69Ad2De90Fe1CD65d198f60781Ad73'
 const addressWhitelist = '0x1EEcF3334A3b9C07048Ed82717b2405A82F88569'
 
+function hash(address: string): string {
+  return ethers.utils.keccak256(
+    ethers.utils.defaultAbiCoder.encode(['address'], [address])
+  )
+}
+
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments } = hre
   const { deploy } = deployments
@@ -15,13 +21,11 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
   const minting = (await ethers.getContract('Minting')) as Minting
   if (
-    (await minting.OGRoot()) !== ethers.utils.keccak256(addressOG) ||
-    (await minting.whitelistRoot()) !== ethers.utils.keccak256(addressWhitelist)
+    (await minting.OGRoot()) !== hash(addressOG) ||
+    (await minting.whitelistRoot()) !== hash(addressWhitelist)
   ) {
-    const txOg = await minting.saveRootOg(ethers.utils.keccak256(addressOG))
-    const txWhite = await minting.saveRootWhitelist(
-      ethers.utils.keccak256(addressWhitelist)
-    )
+    const txOg = await minting.saveRootOg(hash(addressOG))
+    const txWhite = await minting.saveRootWhitelist(hash(addressWhitelist))
 
     await Promise.all([txOg.wait(), txWhite.wait()])
   }
