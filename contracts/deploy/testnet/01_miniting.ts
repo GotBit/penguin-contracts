a68f651232263dc72002a451ade58f87fe3991a7
BuduschiyHokage <kolya.skiba.2013@gmail.com>
2022-02-21 21:56:06 +0300

    change deploy script

--- contracts/deploy/testnet/01_miniting.ts
+++ contracts/deploy/testnet/01_miniting.ts
@@ -1,8 +1,13 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types'
 import { DeployFunction } from 'hardhat-deploy/types'
+import chainlink from '../../chainlink.json'
 
 import { ethers } from 'hardhat'
 import { Minting, PenguinNFT } from '../../typechain'
+
+const chain = chainlink.bsc_testnet
+
+const router = '0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3'
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments } = hre
@@ -13,7 +18,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
   await deploy('Minting', {
     from: deployer.address,
-    args: [nft.address],
+    args: [nft.address, router, chain.VRF, chain.LINK, chain.key, chain.fee],
     log: true,
   })
 }
