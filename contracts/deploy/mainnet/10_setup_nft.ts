bbed164537b0017f1691725a595d2aa46172f597
Gregory Rogachev <kotsmilewtm@gmail.com>
2022-02-22 10:37:54 +0300

    create deploys for mainnet

---
+++ contracts/deploy/mainnet/10_setup_nft.ts
@@ -0,0 +1,29 @@
+import { HardhatRuntimeEnvironment } from 'hardhat/types'
+import { DeployFunction } from 'hardhat-deploy/types'
+
+import { ethers } from 'hardhat'
+import { Minting, PenguinNFT } from '../../typechain'
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments } = hre
+  const { deploy } = deployments
+  const [deployer] = await ethers.getSigners()
+  console.log('Setup nft')
+
+  const nft = (await ethers.getContract('PenguinNFT')) as PenguinNFT
+  const minting = (await ethers.getContract('Minting')) as Minting
+  const minter_role = await nft.MINTER_ROLE()
+
+  if (!(await nft.hasRole(minter_role, minting.address))) {
+    const tx = await nft.setMinter(minting.address)
+    await tx.wait()
+  }
+
+  console.log('Done')
+}
+
+export default func
+func.tags = ['Setup nft']
+func.dependencies = ['NFT']
+func.runAtTheEnd = true
+func.skip = async (hre) => !hre.network.tags.main
---
+++ contracts/deploy/mainnet/11_setup_minting.ts
@@ -0,0 +1,50 @@
+import { HardhatRuntimeEnvironment } from 'hardhat/types'
+import { DeployFunction } from 'hardhat-deploy/types'
+
+import { ethers } from 'hardhat'
+import { Minting, PenguinNFT } from '../../typechain'
+
+const addressOG = '0x4E1602505a69Ad2De90Fe1CD65d198f60781Ad73'
+const addressWhitelist = '0x1EEcF3334A3b9C07048Ed82717b2405A82F88569'
+
+import wallets from '../../wallets.json'
+import merkle from '../../utils'
+
+function hash(address: string): string {
+  return ethers.utils.keccak256(
+    ethers.utils.defaultAbiCoder.encode(['address'], [address])
+  )
+}
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments } = hre
+  const { deploy } = deployments
+  const [deployer, _, bank] = await ethers.getSigners()
+  console.log('Setup minting')
+
+  const og = wallets.OG.map((w) => hash(w))
+  const whitelist = wallets.whitelist.map((w) => hash(w))
+  const ogRoot = merkle.calculateRoot(og)
+  const whitelistRoot = merkle.calculateRoot(whitelist)
+
+  if (!merkle.verify(merkle.generateProof(og, og[0]), ogRoot, og[0])) return
+
+  const minting = (await ethers.getContract('Minting')) as Minting
+  if (
+    (await minting.OGRoot()) !== ogRoot ||
+    (await minting.whitelistRoot()) !== whitelistRoot
+  ) {
+    const txOg = await minting.saveRootOg(ogRoot)
+    const txWhite = await minting.saveRootWhitelist(whitelistRoot)
+
+    await Promise.all([txOg.wait(), txWhite.wait()])
+  }
+
+  console.log('Done')
+}
+
+export default func
+func.tags = ['Setup minting']
+func.dependencies = ['Minting']
+func.runAtTheEnd = true
+func.skip = async (hre) => !hre.network.tags.main
